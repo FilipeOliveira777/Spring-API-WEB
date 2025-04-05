17afc9232bd9512770514429b4a5ca1a1e6366f3
Filipe Oliveira <filipeliradeoliveira05@gmail.com>
2025-04-05 11:22:17 -0300

    Commit inicial criando o controller welcome

---
+++ src/main/java/my_frist_web/api/MyFristApiSpringApplication.java
@@ -0,0 +1,13 @@
+package my_frist_web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFristApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFristApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my_frist_web/api/MyFristApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package my_frist_web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFristApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
